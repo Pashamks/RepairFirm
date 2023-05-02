ff69a4a772cf77e886e9305050c7294b10825d4c
pavlo <pavlo@DESKTOP-SM098C1>
2023-05-02 23:07:02 +0300

    feat: added ef core repository

---
+++ EfCoreRepository/Models/AllModels.cs
@@ -0,0 +1,239 @@
+﻿
+namespace EfCoreRepository.Models
+{
+    public class City
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class Position
+    {
+        public int PositionId { get; set; }
+        public string position { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string country { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class RepairType
+    {
+        public int RepairTypeId { get; set; }
+        public string repairType { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class MaterialColor
+    {
+        public int MaterialColorId { get; set; }
+        public string materialColor { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class RoomType
+    {
+        public int RoomTypeId { get; set; }
+        public string roomType { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class ExaminationPrice
+    {
+        public int ExaminationPriceId { get; set; }
+        public decimal MinSquare { get; set; }
+        public decimal MaxSquare { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class MaterialType
+    {
+        public int MaterialTypeId { get; set; }
+        public string materialType { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class MaterialMeasure
+    {
+        public int MaterialMeasureId { get; set; }
+        public string materialMeasure { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public int CityId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+
+        public virtual City City { get; set; }
+    }
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public int PositionId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+
+        public virtual Position Position { get; set; }
+    }
+    public class Apartment
+    {
+        public int ApartmentId { get; set; }
+        public int ClientId { get; set; }
+        public int CityId { get; set; }
+        public int RoomsCount { get; set; }
+        public string Address { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class MaterialProducer
+    {
+        public int MaterialProducerId { get; set; }
+        public string materialProducer { get; set; }
+        public int CountryId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+
+        public virtual Country Country { get; set; }
+    }
+    public class ExaminationStatus
+    {
+        public int ExaminationStatusId { get; set; }
+        public string examinationStatus { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class HistoryStatus
+    {
+        public int HistoryStatusId { get; set; }
+        public string historyStatus { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public int ApartmentId { get; set; }
+        public int RoomTypeId { get; set; }
+        public decimal FloorSquare { get; set; }
+        public decimal WallsSquare { get; set; }
+        public decimal CellingSquare { get; set; }
+        public int DoorsCount { get; set; }
+        public int WindowsCount { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class Material
+    {
+        public int MaterialId { get; set; }
+        public string MaterialName { get; set; }
+        public int MaterialProducerId { get; set; }
+        public int MaterialColorId { get; set; }
+        public int MaterialTypeId { get; set; }
+        public int MaterialMeasureId { get; set; }
+        public int MaterialVolume { get; set; }
+        public decimal MaterialPrice { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class Repair
+    {
+        public int RepairId { get; set; }
+        public int RepairTypeId { get; set; }
+        public string RepairName { get; set; }
+        public string RepairDescription { get; set; }
+        public decimal RepairMinHours { get; set; }
+        public decimal RepairMaxHours { get; set; }
+        public decimal RepairPrice { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class AreaExamination
+    {
+        public int AreaExaminationId { get; set; }
+        public int EmployeeId { get; set; }
+        public int ExaminationPriceId { get; set; }
+        public int ExaminationStatusId { get; set; }
+        public string Comments { get; set; }
+        public decimal WorkEstimatedPrice { get; set; }
+        public decimal MaterialsEstimatedPrice { get; set; }
+        public DateTime ExaminationDate { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class RoomForExamination
+    {
+        public int RoomForExaminationId { get; set; }
+        public int AreaExaminationId { get; set; }
+        public int RoomId { get; set; }
+        public int? RepairServicesId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class RepairServices
+    {
+        public int RepairServicesId { get; set; }
+        public int? RoomForExaminationId { get; set; }
+        public int HistoryStatusId { get; set; }
+        public int RepairId { get; set; }
+        public int RepairCount { get; set; }
+        public DateTime RepairStartDate { get; set; }
+        public DateTime? RepairEndDate { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class RoomMaterial
+    {
+        public int RoomMaterialId { get; set; }
+        public int RoomForExaminationId { get; set; }
+        public int MaterialId { get; set; }
+        public int MaterialCount { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+
+    public class EmployeeForRepair
+    {
+        public int EmployeeForRepairId { get; set; }
+        public int EmployeeId { get; set; }
+        public int RepairServicesId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+
+
+    public class Contract
+    {
+        public int ContractId { get; set; }
+        public int AreaExaminationId { get; set; }
+        public int HistoryStatusId { get; set; }
+        public int DepartmentId { get; set; }
+        public decimal Prepayment { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime ContractStartDate { get; set; }
+        public DateTime? ContractEndDate { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+}
---
+++ EfCoreRepository/RepairDbContext.cs
@@ -0,0 +1,40 @@
+﻿using EfCoreRepository.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreRepository
+{
+    public class RepairDbContext : DbContext
+    {
+        public DbSet<Repair> Repair { get; set; }
+        public DbSet<RepairType> RepairType { get; set; }
+        public DbSet<RepairType> RepairTypes { get; set; }
+        public DbSet<ExaminationPrice> ExaminationPrice { get; set; }
+        public DbSet<ExaminationStatus> ExaminationStatus { get; set; }
+        public DbSet<AreaExamination> AreaExamination { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Position> Position { get; set; }
+        public DbSet<Room> Room { get; set; }
+        public DbSet<Client> Client { get; set; }
+        public DbSet<Apartment> Apartment { get; set; }
+        public DbSet<RoomType> RoomType { get; set; }
+        public DbSet<RoomForExamination> RoomForExamination { get; set; }
+        public DbSet<Material> Material { get; set; }
+        public DbSet<RoomMaterial> RoomMaterials { get; set; }
+        public DbSet<HistoryStatus> HistoryStatus { get; set; }
+        public DbSet<RepairServices> RepairServices { get; set; }
+        public DbSet<EmployeeForRepair> EmployeeForRepair { get; set; }
+        public DbSet<Contract> Contract { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<MaterialMeasure> MaterialMeasure { get; set; }
+        public DbSet<MaterialType> MaterialType { get; set; }
+        public DbSet<MaterialColor> MaterialColor { get; set; }
+        public DbSet<MaterialProducer> MaterialProducer { get; set; }
+        public DbSet<Country> Country { get; set; }
+        public DbSet<City> City { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-SM098C1;Database=RepairFirma;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
+    }
+}
