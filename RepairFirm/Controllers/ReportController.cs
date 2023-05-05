8b5603870170377f77eefcc9ea84048f2bfb380f
pavlo <pavlo@DESKTOP-SM098C1>
2023-05-05 10:38:58 +0300

    feat: added first normal diagram with iTextSharp to pdf

--- RepairFirm/Controllers/ReportController.cs
+++ RepairFirm/Controllers/ReportController.cs
@@ -3,13 +3,10 @@ using iTextSharp.text.pdf;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
 using System.Data;
-using System.Data.SqlClient;
-using System.Configuration;
-using System.IO;
-using iTextSharp.tool.xml;
-using iTextSharp.text.html.simpleparser;
 using EfCoreRepository;
 using ScottPlot;
+using System.Drawing;
+using RepairFirm.Shared.Models;
 
 namespace RepairFirm.Controllers
 {
@@ -24,8 +21,13 @@ namespace RepairFirm.Controllers
         }
         public IActionResult Index()
         {
-            var list = _dbRepository.GetRepairCountChart();
 
+            if (System.IO.File.Exists("new.pdf"))
+            {
+                return View();
+            }
+
+            
             var list2 = _dbRepository.GetDepartmentServices();
 
 
@@ -59,44 +61,81 @@ namespace RepairFirm.Controllers
             #endregion
 
             var chart4 = _dbRepository.GetEmployeeForRepairType();
+            Bitmap bmp = GenerateImgForRepairsPopular();
 
-            var plt = new ScottPlot.Plot(800, 400);
+            var stream = new FileStream("new.pdf", FileMode.CreateNew);
+
+            var document = new Document(PageSize.A4, 25, 25, 30, 30);
+            var writer = PdfWriter.GetInstance(document, stream);
+            writer.CloseStream = false;
+            document.Open();
+            document.AddAuthor("Pavlo Somko");
+            document.AddTitle("Repair firm analysis report");
+
+            AddPopularRepairDiagram(bmp, document);
+
+            var table = new PdfPTable(2)
+            {
+                WidthPercentage = 100,
+                SpacingBefore = 10,
+            };
+
+            //table.AddCell(new PdfPCell(pdfImage)
+            //{
+            //    Border = 0
+            //});
+            document.Add(table);
+            document.NewPage();
+            document.Add(new Paragraph(new Phrase { new Chunk(("General table").ToUpper()) })
+            {
+                Alignment = Element.ALIGN_CENTER,
+            });
+
+            document.Close();
+            writer.Close();
+
+
+            return View();
+        }
+
+        private Bitmap GenerateImgForRepairsPopular()
+        {
+            var list = _dbRepository.GetRepairCountChart();
+        list.ForEach(x => x.RepairType = x.RepairType.Replace(' ', '\n'));
+
+            var plt = new ScottPlot.Plot(700, 400);
 
             double[] positions = DataGen.Consecutive(list.Count);
             plt.AddBar(list.Select(x => Convert.ToDouble(x.RepairCount)).ToArray(), positions);
 
             plt.XAxis.ManualTickPositions(list.Select((x, i) => (double)(i)).ToArray(), list.Select(x => x.RepairType).ToArray());
-            plt.YAxis.ManualTickPositions(list.Select((x, i) => (double)(x.RepairCount)).ToArray(), 
+            plt.YAxis.ManualTickPositions(list.Select((x, i) => (double)(x.RepairCount)).ToArray(),
                 list.Select(x => x.RepairCount.ToString()).ToArray());
 
-            //plt.YAxis.Ticks(false);
-            //plt.YAxis.Label("Title");
 
-            // vertical axis (when rotated)
-           // plt.XAxis.Ticks(true);
-            plt.XAxis.TickLabelStyle(rotation: 90);
-
-            // horizontal axis (when rotated)
-            //plt.YAxis2.Ticks(true);
-            plt.YAxis2.TickLabelStyle(rotation: 90);
-            //plt.YAxis.TickDensity(1);
-
-            var img = plt.GetImageHTML();
-
-
-            var ChromePdfRenderer = new ChromePdfRenderer();
-
-            //// render html to pdf
-            var PDF = ChromePdfRenderer.RenderHtmlAsPdf(img);
-            //// output to pdf
-            var OutputPath = "HtmlToPDF.pdf";
-            PDF.SaveAs(OutputPath);
-            //var list = _dbRepository.GetRepairCountChart();
-            //var plt = new ScottPlot.Plot(400, 300);
-            //plt.AddScatter(list.Select(x => x.RepairCount).ToArray(),list.Select(x => x.RepairCount).ToArray());
-
-            return View();
+            System.Drawing.Bitmap bmp = plt.Render();
+            return bmp;
         }
+
+        private static void AddPopularRepairDiagram(Bitmap bmp, Document document)
+        {
+            document.NewPage();
+            document.Add(new Paragraph(new Phrase { new Chunk(("The most popular repairs diagram").ToUpper()) })
+            {
+                Alignment = Element.ALIGN_CENTER,
+            });
+            using (var stream2 = new MemoryStream())
+            {
+                bmp.Save(stream2, System.Drawing.Imaging.ImageFormat.Png);
+                stream2.Position = 0;
+                var png = iTextSharp.text.Image.GetInstance(stream2);
+                png.RotationDegrees = 270;
+                png.Rotate();
+                png.Alignment = Element.ALIGN_CENTER;
+                document.Add(png);
+            }
+        }
+
         [HttpPost]
         [Route("GeneratePdf")]
         public IActionResult GeneratePdf()
